0a62f2da7a46454c60bae40ef4723b882e37581a
Yves Beutler <yves.beutler@hotmail.com>
2018-11-23 13:09:14 +0100

    draw a triangle (origin pipeline)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ int main(int argc, char** argv) {
 	SDL_Window* window;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
+	// define the buffer size for each pixel
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 	// define the window
 	window = SDL_CreateWindow("Dilophosaurus", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 
@@ -20,6 +28,15 @@ int main(int argc, char** argv) {
 		// delete old buffer content
 		glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+
+		// draw triangle
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+
+		glEnd();
 
 		// double buffering
 		SDL_GL_SwapWindow(window);
